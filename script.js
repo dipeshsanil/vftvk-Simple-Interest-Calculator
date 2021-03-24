6364a63a1eac959a81695bd5450c62b0ed80edcb
Dipesh Sanil <dipeshsanil24@gmail.com>
2021-03-24 05:40:13 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = parseInt(document.getElementById("years").value);
+    var interest = principal*years*rate/100;
+    var d = parseInt(new Date().getFullYear());
+    var n = d+years;
+
+    if(principal <= 0){ //function to check amount is not negative
+            alert("Enter a positive number");
+            document.getElementById("principal").focus();
+            return false;
+      }
+
+     document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>," + "<br>"+
+                                                    "at an interest rate of <mark>"+ rate+ "%</mark>." +"<br>"+
+                                                    "You will receive an amount of<mark> " + interest + "</mark>,"+"<br>" +
+                                                    "in the year <mark>" + n +"</mark>"
+  
 }
-        
+function slider() {
+    var value = document.getElementById("rate").value;
+    document.getElementById("demo").innerHTML = value + "%";
+   }       
